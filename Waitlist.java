ec873bd22a6b1166a0ecf074b1d3876e8ebefd96
MoustafaY <47026280+MoustafaY@users.noreply.github.com>
2021-10-20 18:14:27 -0500

    Waitlist class has attribute of client id

--- Waitlist.java
+++ Waitlist.java
@@ -3,16 +3,22 @@ import java.io.*;
 public class Waitlist implements Serializable {
   private static final long serialVersionUID = 1L;
   private List<waitListItem> waitItem;
+  private String clientID;
 
-  public Waitlist() {
+  public Waitlist(String cliID) {
     waitItem = new LinkedList<waitListItem>();
+    this.clientID = cliID;
   }
 
 
-  public boolean insertProductToCart(Product product, int quantity) {
+  public boolean insertProductToCart(String clientId, Product product, int quantity) {
     waitListItem item = new waitListItem(product, quantity);
     waitItem.add(item);
     return true;
+  }
+  
+  public String getClientID() {
+	  return clientID;
   }
   
   public Iterator<waitListItem> getWaitListProducts() {
