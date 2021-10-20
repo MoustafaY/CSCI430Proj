ad17da7c1e8b444ae5275483713a1b9eb39054d8
Ujjwal1001 <64380651+Ujjwal1001@users.noreply.github.com>
2021-10-20 16:26:34 -0500

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,81 +1,61 @@
 import java.util.*;
 import java.io.*;
-
 public class Product implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private String name;
+  private String id;
+  private int quantity;
+  private double salePrice;
+  private double supplyPrice;
+  
+  private static final String PRODUCT_STRING = "P";
+  public  Product (String name, int quantity, double salePrice, double supplyPrice) {
+    this.name = name;
+    this.quantity = quantity;
+    this.salePrice = salePrice;
+	this.supplyPrice = supplyPrice;
+    id = PRODUCT_STRING + (ProductIDServer.instance()).getId();
+  }
 
-    private static final long serialVersionUID = 1L;
-    private String productID;
-    private String productname;
-    private float productPrice;
-    private float supplyPrice;
-    private static final String PRODUCT_STRING = "P";
-    private int quantity;
-    private List<Waitlist> waitlistedClients = new LinkedList<Waitlist>();
-    private List<Supplier> sellingSuppliers = new LinkedList<Supplier>();
-
-
-    public Product (String productname, int quantity, float productPrice, float supplyPrice)
-    {
-        this.productname = productname;
-        this.productPrice = productPrice;
-        this.productID = PRODUCT_STRING + (ProductIDServer.instance()).getId();
-        this.quantity = quantity;
-        this.supplyPrice = supplyPrice;
-    }
-
-    public void setProductName(String newName)
-	{
-		this.productname = newName;
-	}
-
-    public void setproductPrice(float newproductprice)
-    {
-        this.productPrice = newproductprice;
-    }
-
-    public String getproductname()
-    {
-        return productname;
-    }
-    public String getproductID()
-    {
-        return productID;
-    }
-    public float getproductPrice()
-    {
-        return productPrice;
-    }
-    public float getsupplyPrice()
-    {
-        return supplyPrice;
-    }
-    public int getQuantity()
-    {
+  public String getName() {
+        return name;
+  }
+  
+  public int getQuantity() {
         return quantity;
-    }
-    public Iterator<Waitlist> getWaitlistedClients()
-    {
-      return waitlistedClients.iterator();
-    }
-    public boolean addToWaitlist(Waitlist w)
-    {
-        return waitlistedClients.add(w);
-    }
-    public boolean addToSellingSuppliers(Supplier s) {
-		return sellingSuppliers.add(s);
-	}
+  }
+  
+  public String getId() {
+        return id;
+  }
+  
+public double getSalePrice() {
+  return salePrice;
+}
+public double getSupplyPrice() {
+	  return supplyPrice; }
+	  
+  public void setName(String newName) {
+        name = newName;
+  }
+  
+  public void setQuantity(int newQuantity) {
+        quantity = newQuantity;
+  }
+  
+  public void setSalePrice(double newSalePrice) {
+	    salePrice = newSalePrice;
+	  }
+	  public void setSupplyPrice(double newSupplyPrice) {
+		supplyPrice = newSupplyPrice;
+  }
+  
+  public boolean equals(String id) {
+        return this.id.equals(id);
+  }
+  
 
-
-    public void setproductQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-    public String toString()
-    {
-        String string = "Product Name: " + productname + ", Product ID: " + productID + ", Product Quantity: " + quantity + ", Product Price: " + productPrice + ", Supply Price: " + supplyPrice;
-        return string; 
-    }
-
-    public void updateQuantity(int q) {
-        setproductQuantity(q);
+  public String toString() {
+        return "Product: " + name + " ID: " + id + " Qty: " + quantity + " salePrice " + salePrice + " supplyPrice " + supplyPrice;
     }
 }
