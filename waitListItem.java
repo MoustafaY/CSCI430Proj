e0f71a155d5e7e86e609ef62fd1aa5f203e6eb4b
MoustafaY <47026280+MoustafaY@users.noreply.github.com>
2021-10-20 17:42:53 -0500

    watilistitem works like shoppingcartitem

---
+++ waitListItem.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class waitListItem implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private Product product;
+  private int quantity;
+
+  public waitListItem(Product p, int q){
+    this.product  = p;
+    this.quantity = q;
+  }
+  
+  public Product getProduct(){
+    return product;
+  }
+
+  public int getQuantity(){
+    return quantity;
+  }
+
+  public void updateQuantity(int newQ){
+    this.quantity = newQ;
+  }
+  
+  public String toString() {
+    return "Product Name: " + product.getName() + ", Quantity: " + quantity;
+  }
+}
