2974667d99a051610eabb44485f5b5dfd953b34e
karkipamit <78070110+karkipamit@users.noreply.github.com>
2021-10-20 01:47:54 -0500

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,71 +1,50 @@
 import java.util.*;
 import java.io.*;
-import java.lang.*;
 
+public class Order implements Serializable {
 
-public class Order implements Serializable{
-  private static final long serialVersionUID = 1L;
-  private double TotalPrice = 0;
-  private List<Product> products = new LinkedList<Product>();
+    private static final long serialVersionUID = 1L;
+    private String clientId;
+    private List itemList = new LinkedList();
 
-  public Order(){}
-
-  public boolean AddProdToOrder(Product product, int quantity, Client client){
-    boolean success = products.add(product);
-    if(success)
-    {
-
-      Iterator<Supplier> suppliers = product.getSuppliers();
-      Supplier temp_s;
-      int TempQuantity = 0;
-      double temp_p;
-      while ((suppliers.hasNext()))
-      {
-        temp_s = suppliers.next();
-        TempQuantity = temp_s.getQuantity();
-        if (TempQuantity > 0)
-        {
-          if (TempQuantity >= quantity)
-          {
-            temp_s.setNewQuantity(quantity);
-            temp_p = temp_s.getPrice();
-            this.TotalPrice = TotalPrice + (quantity * temp_p);
-            quantity = 0;
-          }
-          else
-          {
-            quantity = quantity - TempQuantity;
-            temp_s.setNewQuantity(TempQuantity);
-            temp_p = temp_s.getPrice();
-            this.TotalPrice = TotalPrice + (TempQuantity * temp_p);
-          }
-        }
-      }
-
-      if (quantity != 0)
-      {
-        Waitlist w = new Waitlist(client, product, quantity);
-        success = client.addProductToWaitlist(w);
-        success = product.addClientToWaitlist(w);
-        if (success){
-          System.out.println("Order successful, but " + quantity + " were added to a waitlist.");
-        }
-        else{
-          System.out.println("Order successful, but products weren't successfully added to a waitlist.");
-        }
-      }
-
-      return true;
+    public Order(String clientId) {
+        this.clientId = clientId;
     }
-    else
-    {
-      return false;
+
+
+    public Iterator getItemList(){
+        return itemList.iterator();
     }
-  }
+  public List getItemLists(){
+        return itemList;
+    }
 
-  public double getTotal(){
-    return TotalPrice;
-  }
+    public void addItem(Item i) {
+        itemList.add(i);
+    }
 
+    public String getClientId() {
+        return clientId;
+    }
 
+    public Item find(String clientId) {
+        for (Iterator iterator = itemList.iterator(); iterator.hasNext();) {
+            Item item = (Item) iterator.next();
+            if (clientId.equals(item.getProductId())) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void printOrder() {
+        for (int i = 0; i < itemList.size(); i++) {
+            System.out.println(itemList.get(i));
+        }
+
+    }
+
+    public String toString() {
+        return "Test";
+    }
 }
