41a32e99aeebeb01ae3b7eade92ea2a98e73459b
karkipamit <78070110+karkipamit@users.noreply.github.com>
2021-10-20 02:11:14 -0500

    Create Waitlist.java
    created a waitlist for product on client order

---
+++ Waitlist.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Waitlist implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private Product product;
+  private Client client;
+  private int quantity;
+
+  public Waitlist(Client c, Product p, int q){
+    this.client   = c;
+    this.product  = p;
+    this.quantity = q;
+  }
+
+  public Waitlist(Client c, int q) {
+     this.client = c;
+     this.quantity = q;
+    }
+
+  public Product getProduct(){
+    return product;
+  }
+
+  public Client getClient(){
+    return client;
+  }
+
+  public int getQuantity(){
+    return quantity;
+  }
+
+  public void updateQuantity(int newQ){
+    this.quantity = newQ;
+  }
+  
+  public String toString() {
+    return client.toString() + "Quantity: " + quantity + product.toString();
+  }
+  public String toStringClient() {
+    return "Product Name: " + product.getproductname() + ", Quantity: " + quantity;
+  }
+}
