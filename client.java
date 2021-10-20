3973703d3a02d7058fcb5e57e2ccd98fa8ab2fa8
Ujjwal1001 <64380651+Ujjwal1001@users.noreply.github.com>
2021-10-20 16:00:06 -0500

    Update client.java
    updated client class

--- client.java
+++ client.java
@@ -4,6 +4,10 @@ import java.io.*;
 public class client implements Serializable{
 	private String name;
 	private String id;
+	private double balance;
+        private ShoppingCart shoppingCart;
+        private TransactionList transactionList;
+
 	private static final String CLIENT_STRING = "M";
 	
 	public client(String name) {
@@ -18,13 +22,38 @@ public class client implements Serializable{
 	public String getId() {
 		return id;
 	}
-	
+	public double getBalance() {
+        return balance;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public TransactionList getTransactionList() {
+        return transactionList;
+    }
 	public void setName(String newName) {
 		name = newName;
 	}
-	
+	 public void setShoppingCart(ShoppingCart cart) {
+	        shoppingCart = cart;
+	    }
+
+	    public void setBalance(double newBalance) {
+	        balance = newBalance;
+	    }
+
+	    public void addBalance(double bal) {
+	        balance += bal;
+	    }
+
+	    public void subtractBalance(double bal) {
+	        balance -= bal;
+	    }
+
 	public String toString() {
-		String string = "Client name " + name + " Id " + id;
+		String string = "Client name " + name + " Id " + id + " Client balance: " + balance;
 		return string;
 	}
 	
